6bd8e0de93e9b1589e180a2d40d6d520f5ae7fe4
nidhal1508 <nidhal1508@gmail.com>
2020-12-09 15:43:04 +0100

    install & import node mailer & create email.js (send email) challenge03 final

--- routers/email.js
+++ routers/email.js
@@ -1,17 +1,34 @@
- const email = require('nodemailer');
+ const nodemailer = require('nodemailer');
  const express = require('express');
+const {google} = require('googleapis')
  const router = express.Router();
 
+//  let transporter = email.createTransport({
+//     service: 'gmail',
+//     auth :{
+//         xoauth2:xoauth2.createXOAuth2Generator({
+//         useremail:"nidhal1508@gmail.com",
+//         clientId: '99225458119-k1ov3fkf46euq9vlevjh4s1vtotf9tm0.apps.googleusercontent.com',
+//         clientSecret: 'slPtz400_oxVIv6ZIbmnKTtz',
+//         refreshToken: '1//04TolYpeo3k02CgYIARAAGAQSNwF-L9IrMcBX-fu-LXi3OBz-52JbJs2uoumHo5B-9KvixOvPlZFPTaaEsRKUHXojwjR_BrrXtSQ',
+//         accessToken:'ya29.a0AfH6SMAryjZoTcT0kCSV0IzUInpntczJ0lnPfpByH-NqQJloJezXs1Uo-n3m_tTZm1CL6UU4ULa5zHNsxEwBIga6LxUH4m5qeZMscPA4Jk0EyrU0SlGN4usmlD-uB6j_uCBG0fJ5asB1rxvQeY0FCqgNdzXh7sBnoU3icji-UJQ'
+//     })
+//     }
 
-//create transporter
-let transporter = email.createTransport({
-    service: 'gmail',
-    auth :{
-useremail:process.env.Email,
-pass:process.env.Password
+// })
 
-    }
-})
+
+
+       
+        const redirect ='https://developers.google.com/oauthplayground';
+      const  clientId= '99225458119-k1ov3fkf46euq9vlevjh4s1vtotf9tm0.apps.googleusercontent.com';
+      const  clientSecret= 'slPtz400_oxVIv6ZIbmnKTtz';
+       const refreshToken= '1//04TolYpeo3k02CgYIARAAGAQSNwF-L9IrMcBX-fu-LXi3OBz-52JbJs2uoumHo5B-9KvixOvPlZFPTaaEsRKUHXojwjR_BrrXtSQ';
+     
+    
+const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirect)
+oAuth2Client.setCredentials({refresh_token: refreshToken})
+
 
 //emaail option & content
 
@@ -19,17 +36,33 @@ let mailOptions={
     from:"nidhal1508@gmail.com",
     to:"nidhaltest1508@gmail.com",
     subject:"testNodemail",
-    text:"test working"
+    text:"test working",
+    html:"<h1>test working html</h1>"
 };
 
 //send email
-transporter.sendMail(mailOptions, function (err , data) {
-    if (err) {
-        console.log("errors occurs", err)
-    } else {
-        console.log("email sent")
+async function sendMail(params) {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'nidhal1508@gmail.com',
+                clientId: clientId,
+                clientSecret: clientSecret,
+                refreshToken: refreshToken,
+                accessToken: accessToken
+            }
+        })
+        const result= await transport.sendMail(mailOptions) ;
+        return result;
+    } catch (error) {
+        return error
     }
-});
+}
+
+sendMail().then((result)=> console.log('email sent') ).catch((error)=> console.log('error.message'))
 
 //export router 
 module.exports = router;
